a080a1fe2b1dd0ac6862ab8819418a16e273f0c4
veenagurrala16 <158726922+veenagurrala16@users.noreply.github.com>
2024-02-21 00:54:32 +0530

    product.py

--- product.py
+++ product.py
@@ -26,32 +26,32 @@ with st.expander('Analyze CSV'):
         return blob1.sentiment.polarity
 
 #
-   def analyze(x):
-        if x >= 0.5:
-            return 'Positive'
-        elif x <= -0.5:
-            return 'Negative'
-        else:
-            return 'Neutral'
+def analyze(x):
+    if x >= 0.5:
+        return 'Positive'
+    elif x <= -0.5:
+        return 'Negative'
+    else:
+        return 'Neutral'
 
 #
-    if upl:
-        df = pd.read_excel(upl)
-        del df['Unnamed: 0']
-        df['score'] = df['tweets'].apply(score)
-        df['analysis'] = df['score'].apply(analyze)
-        st.write(df.head(10))
+if upl:
+    df = pd.read_excel(upl)
+    del df['Unnamed: 0']
+    df['score'] = df['tweets'].apply(score)
+    df['analysis'] = df['score'].apply(analyze)
+    st.write(df.head(10))
 
-        @st.cache
-        def convert_df(df):
+@st.cache
+def convert_df(df):
             # IMPORTANT: Cache the conversion to prevent computation on every rerun
-            return df.to_csv().encode('utf-8')
+    return df.to_csv().encode('utf-8')
 
-        csv = convert_df(df)
+    csv = convert_df(df)
 
-        st.download_button(
-            label="Download data as CSV",
-            data=csv,
-            file_name='sentiment.csv',
-            mime='text/csv',
-        )
+    st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name='sentiment.csv',
+    mime='text/csv',
+    )
